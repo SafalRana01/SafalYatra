107d5d3cb053d0b09db85d3beb4f5edb8c577b98
SafalRana01 <magarsafal61@gmail.com>
2025-03-08 22:03:09 +0545

    Colors configuration for background,button is here

---
+++ Frontend/safal_yatra/lib/components/appColors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backGroundColor = Color(0xFFF3F2FA);
+  static const Color buttonColor = Color(0xFFFF57C00);
+  // static const Color buttonColor = Color(0xFFCC4500);
+  // static const Color buttonColor = Color(0xFF4F7480);
+  // static const Color buttonColor = Color(0xFF356DA9);
+
+
+  static const Color tryColor = Color(0xFF356DA9);
+}
