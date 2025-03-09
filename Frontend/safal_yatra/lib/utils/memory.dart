5780254a42dcdcf2261c1a35ba07c159d6d3517e
SafalRana01 <magarsafal61@gmail.com>
2025-03-09 14:12:11 +0545

    Memory class to handle token and role storage with SharedPreferences

---
+++ Frontend/safal_yatra/lib/utils/memory.dart
@@ -0,0 +1,29 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Memory {
+  static SharedPreferences? prefs;
+
+  static Future<void> init() async {
+    prefs = await SharedPreferences.getInstance();
+  }
+
+  static void saveToken(String token) async {
+    await prefs?.setString('token', token);
+  }
+
+  static String? getToken() {
+    return prefs?.getString('token');
+  }
+
+  static void saveRole(String role) async {
+    await prefs?.setString('role', role);
+  }
+
+  static String? getRole() {
+    return prefs?.getString('role');
+  }
+
+  static void clear() async {
+    await prefs?.clear();
+  }
+} 
